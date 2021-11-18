da80ac3ab329bbc77b6322c7138bb008bf64b56d
Dan Skaggs <dan@skaggsfamily.ws>
2021-11-18 08:35:30 -0500

    Added new feature to our shiny node app

---
+++ libs/server/node-app-one/feature/some-lib-three/src/lib/server-node-app-one-feature-some-lib-three.spec.ts
@@ -0,0 +1,9 @@
+import { serverNodeAppOneFeatureSomeLibThree } from './server-node-app-one-feature-some-lib-three';
+
+describe('serverNodeAppOneFeatureSomeLibThree', () => {
+  it('should work', () => {
+    expect(serverNodeAppOneFeatureSomeLibThree()).toEqual(
+      'server-node-app-one-feature-some-lib-three'
+    );
+  });
+});
